348df193ed32fa2e2d7c0ab85d14799faeeadba7
Thanos Trompoukis <atr0x23@gmail.com>
2020-12-26 12:16:19 +0000

    erase an extra parenthesis on line 17 between right arrow and res on game.js

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ let availableQuesions = [];
 let questions = [];
 
 fetch('https://opentdb.com/api.php?amount=20&category=9&difficulty=medium&type=multiple')
-.then(res) => {
+.then(res => {
     return res.json(); 
 })
 .then(loadedQuestions) => {
