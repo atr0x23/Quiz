6296d0e025f9cb88443318f7dbca09573423b925
atr0x23 <atr0x23@gmail.com>
2023-11-16 11:08:46 +0200

    assigned to football questions

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ let availableQuesions = [];
 let questions = [];
 
 //fetch('https://opentdb.com/api.php?amount=20&category=9&difficulty=medium&type=multiple')
-fetch('questions.json')
+fetch('football.json')
 .then(res => {
     return res.json(); 
 })
